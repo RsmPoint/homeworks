ddae7ba3ddaca02db3a4ac86e5cd1f9c5ddc7f78
Aleksander Apostolidis <alex.ca.dev@gmail.com>
2020-04-11 21:38:00 +0200

    Add homework12

---
+++ Homework12/Homework12.js
@@ -0,0 +1,32 @@
+/*****************************
+* Homework12 01
+*/
+
+/*
+Mark and John are trying to compare their BMI (Body Mass Index), which is calculated using the formula: BMI = mass / height^2 = mass / (height * height). (mass in kg and height in meter).
+
+1. Store Mark's and John's mass and height in variables
+2. Calculate both their BMIs
+3. Write IF condition to output to console who has a higher BMI, if Mark has higher BMI ouptu "Mark has higher BMI", if John has higher BMI output "John has higher BMI" ,
+if they have equal BMI output " Jhon and Mark has same BMI"
+
+*/
+
+/*****************************
+* Homework12 02
+*/
+
+/*
+John and Mike both play basketball in different teams. In the latest 3 games, John's team scored 89, 120 and 103 points, while Mike's team scored 116, 94 and 123 points.
+
+1. Calculate the average score for each team
+2. Decide which teams wins in average (highest average score), and print the winner to the console. Also include the average score in the output.
+3. Then change the scores to show different winners. Don't forget to take into account there might be a draw (the same average score)
+
+4. EXTRA: Mary also plays basketball, and her team scored 97, 134 and 105 points. Like before, log the average winner to the console.
+HINT: you will need the && operator to take the decision.
+If you can't solve this one, just watch the solution, it's no problem :)
+5. Like before, change the scores to generate different winners, keeping in mind there might be draws.
+
+GOOD LUCK 😀
+*/
